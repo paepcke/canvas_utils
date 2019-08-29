074de5019f6b417a18d1e4582804ef4a104672f3
Andreas Paepcke <paepcke@cs.stanford.edu>
2019-08-29 14:45:21 -0700

    Renamed cron script to cron_run_canvas_refresh.bash; marked space for raw-data refresh script call.

--- Scripts/cron_run_canvas_refresh.bash
+++ Scripts/cron_run_canvas_refresh.bash
@@ -3,8 +3,6 @@
 # Usually called from cron. 
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# Go to project root dir:
-cd $SCRIPT_DIR/..
 
 # Create the ~/cronlogs dir if needed:
 if [[ ! -e $HOME/cronlogs ]]
@@ -12,10 +10,16 @@ then
     mkdir $HOME/cronlogs
 fi
 
-# Activate the proper anaconda environment; if that
-# succeeds, run canvas_prep.py, creating a new log
-# file for its output:
+# To the canvas_utils project root:
+cd $SCRIPT_DIR/..
 
+# Run the canvas raw data refresh into canvasdata_prd.  If that
+# succeeds, activate the proper anaconda environment; if that
+# succeeds, run canvas_prep.py, creating a new log file for its
+# output:
+
+# <Karen's invocation of raw data refresh> && \
+# Go to project root dir:
 $HOME/anaconda3/bin/activate canvas_utils && \
 $HOME/anaconda3/envs/canvas_utils/bin/python src/canvas_utils/canvas_prep.py > \
 $HOME/cronlogs/cron_aux_refresh_$(/bin/date +%d-%m-%Y).log 2>&1
