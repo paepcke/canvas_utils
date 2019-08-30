765801ed6ddc77ab0aa85e50fcf1a1739a6e4afb
Andreas Paepcke <paepcke@cs.stanford.edu>
2019-08-30 09:37:25 -0700

    In cron script, moved 'cd to project root' to just after placeholder for Karen.

--- Scripts/cron_run_canvas_refresh.bash
+++ Scripts/cron_run_canvas_refresh.bash
@@ -16,9 +16,6 @@ then
     mkdir $HOME/CanvasTableCopies
 fi
 
-# To the canvas_utils project root:
-cd $SCRIPT_DIR/..
-
 LOG_PATH=$HOME/cronlogs/cron_aux_refresh_$(/bin/date +%d-%m-%Y).log
 
 # Run the canvas raw data refresh into canvasdata_prd.  If that
@@ -26,8 +23,9 @@ LOG_PATH=$HOME/cronlogs/cron_aux_refresh_$(/bin/date +%d-%m-%Y).log
 # succeeds, run canvas_prep.py, creating a new log file for its
 # output:
 
-# <Karen's invocation of raw data refresh> && \
-# Go to project root dir:
+# <Karen's invocation of raw data refresh goes here> && \
+# To the canvas_utils project root:
+cd $SCRIPT_DIR/.. && \
 $HOME/anaconda3/bin/activate canvas_utils && \
     $HOME/anaconda3/envs/canvas_utils/bin/python src/canvas_utils/canvas_prep.py > $LOG_PATH 2>&1 && \
     src/canvas_utils/copy_aux_tables.py --destdir ${HOME}/CanvasTableCopies >> $LOG_PATH 2>&1
